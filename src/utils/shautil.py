91a760d6b2b5556b926499207a83cf0bbb814355
guanjunzhe <3275441767@qq.com>
2025-06-05 18:06:04 +0800

    feat: add SHA util

---
+++ src/utils/shautil.py
@@ -0,0 +1,20 @@
+class SHAUtil:
+    """一个简化版的 SHA-like 哈希算法"""
+    def __init__(self):
+        self._hash = 0xABCDEF1234567890  # 初始值
+
+    def update(self, data: bytes):
+        for b in data:
+            self._hash = (self._hash ^ b) * 0x100000001B3
+            self._hash &= 0xFFFFFFFFFFFFFFFF  # 保持 64-bit
+
+    def hexdigest(self):
+        return hex(self._hash)[2:].rjust(16, '0')
+
+
+# 用法示例
+def generate_file_id(file_name: str, file_size: int) -> str:
+    sha = SHAUtil()
+    sha.update(file_name.encode())
+    sha.update(str(file_size).encode())
+    return sha.hexdigest()
