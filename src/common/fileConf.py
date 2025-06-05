6013aaf89c24da1350462c671739d43499ef2588
guanjunzhe <3275441767@qq.com>
2025-06-05 17:27:11 +0800

    fix: add file type to file info

--- src/common/fileConf.py
+++ src/common/fileConf.py
@@ -10,11 +10,12 @@ class ShareType(enum.Enum):
 
 class FileInfo:
     """文件信息"""
-    def __init__(self, name: str, size: int, path: str, host: str):
-        self.name = name  # 文件名
-        self.size = size  # 文件大小（字节）
-        self.path = path  # 文件路径
-        self.host = host  # 文件所在主机(ip地址+端口)
+    def __init__(self, name: str, size: int, path: str, host: str, file_type: ShareType = ShareType.OTHER):
+        self.name = name        # 文件名
+        self.size = size        # 文件大小（字节）
+        self.path = path        # 文件路径
+        self.host = host        # 文件所在主机(ip地址+端口)
+        self.type = file_type   # 文件类型
 
     def __repr__(self):
-        return f"FileInfo(name={self.name}, size={self.size}, path={self.path})"
+        return f"FileInfo(name={self.name}, size={self.size}, path={self.path}, host={self.host}, type={self.type})"
